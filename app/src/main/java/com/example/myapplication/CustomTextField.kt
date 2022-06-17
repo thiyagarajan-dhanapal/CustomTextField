a00dd170168beda5a494499ec1cca063e33ce7e8
Thiyagarajan Dhanapal <thiyagarajan.dhanapal@byjus.com>
2022-06-17 13:50:56 +0530

    initial commit for the CustomTextField.kt

---
+++ app/src/main/java/com/example/myapplication/CustomTextField.kt
@@ -0,0 +1,148 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.TextFieldDefaults.indicatorLine
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+
+@OptIn(
+    ExperimentalMaterialApi::class, ExperimentalMaterialApi::class,
+    ExperimentalMaterialApi::class
+)
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    label: @Composable (() -> Unit)? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    startDrawable: Int = -1,
+    trailingIcon: @Composable (() -> Unit)? = null,
+) {
+
+    Row(horizontalArrangement = Arrangement.Center) {
+        if (startDrawable > -1) {
+            Icon(
+                modifier = Modifier
+                    .padding(end = 10.dp)
+                    .align(Alignment.Bottom),
+                painter = painterResource(id = startDrawable),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+        }
+
+        // parameters below will be passed to BasicTextField for correct behavior of the text field,
+        // and to the decoration box for proper styling and sizing
+        val enabled = true
+        val singleLine = true
+        val colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent)
+
+        NewTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = modifier.clickable {MutableInteractionSource() },
+            label = label,
+            trailingIcon = trailingIcon,
+            interactionSource=interactionSource,
+            contentPadding = TextFieldDefaults.textFieldWithoutLabelPadding(start = 0.dp, end = 0.dp, top = 0.dp)
+        )
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun NewTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    singleLine: Boolean = false,
+    maxLines: Int = Int.MAX_VALUE,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = androidx.compose.material.MaterialTheme.shapes.small.copy(bottomEnd = ZeroCornerSize, bottomStart = ZeroCornerSize),
+    colors: TextFieldColors = TextFieldDefaults.textFieldColors(),
+    contentPadding: PaddingValues =
+        if (label == null) {
+            TextFieldDefaults.textFieldWithoutLabelPadding()
+        } else {
+            TextFieldDefaults.textFieldWithLabelPadding()
+        }) {
+    // If color is not provided via the text style, use content color as a default
+    val textColor = textStyle.color.takeOrElse {
+        colors.textColor(enabled).value
+    }
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+
+    @OptIn(ExperimentalMaterialApi::class)
+    BasicTextField(
+        value = value,
+        modifier = modifier
+            .background(colors.backgroundColor(enabled).value, shape)
+            .indicatorLine(enabled, isError, interactionSource, colors)
+            .defaultMinSize(
+                minWidth = TextFieldDefaults.MinWidth,
+                minHeight = TextFieldDefaults.MinHeight
+            ),
+        onValueChange = onValueChange,
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = mergedTextStyle,
+        cursorBrush = SolidColor(colors.cursorColor(isError).value),
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        interactionSource = interactionSource,
+        singleLine = singleLine,
+        maxLines = maxLines,
+        decorationBox = @Composable { innerTextField ->
+            // places leading icon, text field with label and placeholder, trailing icon
+            TextFieldDefaults.TextFieldDecorationBox(
+                value = value,
+                visualTransformation = visualTransformation,
+                innerTextField = innerTextField,
+                placeholder = placeholder,
+                label = label,
+                leadingIcon = leadingIcon,
+                trailingIcon = trailingIcon,
+                singleLine = singleLine,
+                enabled = enabled,
+                isError = isError,
+                interactionSource = interactionSource,
+                colors = colors,
+            )
+        }
+    )
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,168 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.myapplication.R.drawable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    val studentName = rememberSaveable { mutableStateOf("") }
+                    val studentEmail = rememberSaveable { mutableStateOf("") }
+                    val studentLocation = rememberSaveable { mutableStateOf("") }
+                    val studentGrade = rememberSaveable { mutableStateOf("") }
+                    val schoolName = rememberSaveable { mutableStateOf("") }
+                    val dummy = rememberSaveable { mutableStateOf("") }
+                    Column {
+                        Text(
+                            text = "Help us personalise\nyour learning experience",
+                            style = MaterialTheme.typography.headlineMedium.copy(textAlign = TextAlign.Center),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 32.dp)
+                                .align(Alignment.CenterHorizontally)
+                        )
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(24.dp),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 32.dp, start = 35.dp, end = 35.dp)
+                        ) {
+
+
+                            var interactionSource =remember { MutableInteractionSource() }
+                            CustomTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight(),
+                                value = studentName.value,
+                                onValueChange = { studentName.value = it },
+                                interactionSource = interactionSource,
+                                label = {
+                                    Text(
+                                        text = "Student’s Name*",
+                                        style = if (interactionSource.collectIsFocusedAsState().value) MaterialTheme.typography.bodySmall else MaterialTheme.typography.bodyLarge
+                                    )
+                                },
+                                startDrawable = drawable.ic_onboard_student
+                            )
+                            TextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight(),
+                                value = dummy.value,
+                                onValueChange = { dummy.value = it },
+                                label = { Text(text = "Student’s Name*") },
+                            )
+                            CustomTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight(),
+                                value = studentEmail.value,
+                                onValueChange = { studentEmail.value = it },
+                                label = { Text(text = "Enter your Email ID *") },
+                                startDrawable = drawable.ic_onboard_email
+                            )
+                            CustomTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight(),
+                                value = studentLocation.value,
+                                onValueChange = { studentLocation.value = it },
+                                label = { Text(text = "Select your location *") },
+                                startDrawable = drawable.ic_onboard_location
+                            )
+                            CustomTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight(),
+                                value = studentGrade.value,
+                                onValueChange = { studentGrade.value = it },
+                                label = { Text(text = "Select Grade/Course *") },
+                                startDrawable = drawable.ic_onboard_grade
+                            )
+                            CustomTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight(),
+                                value = schoolName.value,
+                                onValueChange = { schoolName.value = it },
+                                label = { Text(text = "Enter School Name *") },
+                                startDrawable = drawable.ic_onboard_school
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = drawable.ic_arrow_down),
+                                    contentDescription = null
+                                )
+                            }
+
+
+                        }
+
+                        Button(
+                            onClick = {
+                                Toast.makeText(
+                                    this@MainActivity, "button pressed", Toast.LENGTH_LONG
+                                ).show()
+                            },
+                            modifier = Modifier
+                                .padding(top = 52.dp)
+                                .align(Alignment.CenterHorizontally),
+                            shape = RoundedCornerShape(8.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.gray))
+                        ) {
+                            Text(
+                                text = "Start Learning",
+                                color = Color.White,
+                                modifier = Modifier.padding(
+                                    top = 8.dp, bottom = 8.dp, start = 16.dp, end = 16.dp
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
+
+
